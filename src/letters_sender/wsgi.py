55b0c6b9574d240b8613ce22165eb59743fa9d03
Max Zakirov <zakirov_mv@srvhub.ru>
2021-02-07 15:20:38 +0300

    init project

---
+++ src/letters_sender/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for letters_sender project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'letters_sender.settings')
+
+application = get_wsgi_application()
